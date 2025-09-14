d786676ad94032a9f90f6612a30eb1fe6270bfea
sebastian-reyes <sebastianreyes2105@gmail.com>
2025-09-14 12:17:33 -0500

    testing hot reload, add properties in code

--- code-with-quarkus/src/main/java/com/sreyes/ExampleResource.java
+++ code-with-quarkus/src/main/java/com/sreyes/ExampleResource.java
@@ -2,15 +2,27 @@ package com.sreyes;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/hello")
 public class ExampleResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
-    }
+  @ConfigProperty(name = "greeting")
+  String greeting;
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello() {
+    return "Hello from Quarkus REST";
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  @Path("/custom/{name}")
+  public String helloCustom(@PathParam("name") String name) {
+    return String.format("%s %s from Quarkus, testing Hot Reload!", greeting, name);
+  }
 }
